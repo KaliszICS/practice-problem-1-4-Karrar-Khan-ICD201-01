5a035243e2376a6258b881eb6d0876f7ffdd906a
Karrar-Khan-ICD201-01 <Karrar.khan@student.tdsb.on.ca>
2024-11-05 10:08:21 -0500

    Update PP1_4.py

--- PP1_4.py
+++ PP1_4.py
@@ -1,30 +1,24 @@
-
-
 def q1():
-  word = input("Input a word: ")
-
-print (word)
+    word = input("Input a word: ")
+    print (word)
 
 def q2():
-name = input("Input a name: ")
-
-print ("Hello"+ " " + name)
+    name = input("Input a name: ")
+    print ("Hello "+ name)
 
 def q3():
-name = input("Input a name: ")
-lname = input ("Input your last name: ")
-
-print (lname)
-print (name)
+    name = input("Input a name: ")
+    lname = input ("Input your last name: ")
+    print (f"{lname} {name}")
+    
 def q4():
-student1 = input ("Input a student: ")
-student2 = input ("Input another student: ")
-
-print ("Your students are" + " "+  student1 +" "+ "and" + " "+ student2)
+    student1 = input ("Input a student: ")
+    student2 = input ("Input another student: ")
+    print ("Your students are " +  student1 + " and " + student2)
 
 #Do not edit code below this comment
 
-#q1()
-#q2()
-#q3()
-#q4()
+# q1()
+# q2()
+# q3()
+# q4()
